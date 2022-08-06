7171453c7d334076eca8b38f4b9c81b2d227a763
Taeuk Um <42173347+umtuk@users.noreply.github.com>
2022-08-06 21:59:06 +0900

    fix app.js

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const app = express();
 var path = require('path');
 
 const fs = require('fs');
-// const options = {
-//     key : fs.readFileSync('./ssl/private.key'), // 개인키
-//     cert : fs.readFileSync('./ssl/certificate.crt'), // 서버인증서
-//     ca : fs.readFileSync('./ssl/ca_bundle.crt'), // 루트체인
-// };
+const options = {
+    key : fs.readFileSync('./ssl/private.key'), // 개인키
+    cert : fs.readFileSync('./ssl/certificate.crt'), // 서버인증서
+    ca : fs.readFileSync('./ssl/ca_bundle.crt'), // 루트체인
+};
 
 const indexRouter = require('./routes/index');
 const emailRouter = require('./routes/indexEmail');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
+app.use('/', indexRouter);
 app.get('/', async function(req, res, next){
     res.render('../views/tutorials.ejs', { 
         page : 0,
@@ -33,4 +33,4 @@ app.use('/mturk', indexRouter);
 app.use('/marker', emailRouter);
 
 http.createServer(app).listen(3000);
-// https.createServer(options, app).listen(3001);
+https.createServer(options, app).listen(3001);
