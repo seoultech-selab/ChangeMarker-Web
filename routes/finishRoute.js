22ac0c378a399ebcc8074b2fb08f42fb9aabbbb6
Taeuk Um <42173347+umtuk@users.noreply.github.com>
2022-08-18 20:51:19 +0900

    Update finish check at least 1 script each change

--- routes/finishRoute.js
+++ routes/finishRoute.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const router = express.Router();
+
 const userService = require('../src/domain/user/userService');
+const scriptsWebService = require('../src/domain/scripts_web/scriptsWebService');
+
 
 router.get('/check', async function(req, res, next) {
     let user = await userService.getByWorkerId(req.session.workerId);
+    let scripts = await scriptsWebService.getByUserCode(user.userCode);
+
+    let changeIdSet = new Set();
+
+    for (var script of scripts) {
+        changeIdSet.add(script.chaingId);
+    }
+
+    if (changeIdSet.size != 5) {
+        res.status(400).send(); return;
+    }
 
     if (user.status == 'started') {
         await userService.putStatusByWorkerId('finished', user.workerId);
-        res.status(200).send();
+        res.status(200).send(); return;
     }
     else if (user.status == 'finished') {
-        res.status(200).send();
+        res.status(200).send(); return;
     }
     else {
-        res.status(500).send();
+        res.status(500).send(); return;
     }
 });
 
