040e83f870a1bfa4ca16200b715eae8e86bb18ee
Taeuk Um <42173347+umtuk@users.noreply.github.com>
2022-08-24 13:20:04 +0900

    hot fix - update /delete route error exception

--- routes/deleteRoute.js
+++ routes/deleteRoute.js
@@ -6,29 +6,54 @@ router.use('/', function(req, res, next) {
     let config = require('../db/db_info');
     let pool = mysql.createPool(config);
 
+    if (!req.body) {
+        res.status(500).send();
+        return;
+    }
+
     pool.getConnection(function(err, conn) {
         if (!err) {
             let query = "DELETE FROM scripts_web where `user_code`='";
+
+            if (!req.body.user_code) {
+                res.status(500).send();
+                conn.release();
+                return;
+            }
+
             query += req.body.user_code;
             query += "' and `type`='";
+
+            if (!req.body.type) {
+                res.status(500).send();
+                conn.release();
+                return;
+            }
+
             query += req.body.type;
-            if (req.body.length_old != 0) {
+            if (req.body?.length_old != 0) {
                 query += "' and `length_old`='";
                 query += req.body.length_old;
             }
-            if (req.body.line_number_old.length != 0) {
+            if (req.body?.line_number_old?.length != 0) {
                 query += "' and `line_number_old`='";
                 query += req.body.line_number_old;
             }
-            if (req.body.length_new != 0) {
+            if (req.body?.length_new != 0) {
                 query += "' and `length_new`='";
                 query += req.body.length_new;
             }
-            if (req.body.line_number_new != 0) {
+            if (req.body?.line_number_new != 0) {
                 query += "' and `line_number_new`='";
                 query += req.body.line_number_new;
             }
             query += "' and `change_id`='";
+
+            if (!req.body.change_id) {
+                res.status(500).send();
+                conn.release();
+                return;
+            }
             query += req.body.change_id;
             query += "';"
             conn.query(query, (err, result) => {
